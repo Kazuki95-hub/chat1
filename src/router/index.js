e78e05658abef2fbf9af2e0a4f1f9d6bc996e611
kazuki komatsu <spitzspitzspitz14@icloud.com>
2021-05-09 11:29:34 +0900

    first-commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import store from '../store/index';
 
 Vue.use(VueRouter)
 
@@ -17,13 +20,39 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path:'/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path:'/register',
+    name:'Register',
+    component:Register
   }
+
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if(
+    to.matched.some((record) => record.meta.requiresAuth )&& !store.state.auth 
+  ){
+    next({
+      path:"/",
+      query:{
+        redirect: to.fullPath,
+      },
+    });
+  }else {
+    next();
+  }
+});
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,64 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios';
+import createPertistedState from "vuex-persistedstate";
+import router from "../router/index";
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  plugins:[createPertistedState()],
   state: {
+    auth: "",
+    user:"",
   },
   mutations: {
+    auth(state,payload) {
+      state.auth = payload;
+    },
+    user(state,payload) {
+      state.user = payload;
+    },
+    logout(state, payload) {
+      state.auth = payload;
+    },
+    changeUserData(state,payload) {
+      state.user.profile = payload;
+    },
   },
   actions: {
+    async login({commit},{email, password}) {
+      const responseLogin = await axios.post(
+        "",
+        {
+          email: email,
+          password: password,
+        }
+      );
+      const responseUser = await axios.post(
+        "",
+        {
+          params: {
+            email:email,
+          },
+        }
+      );
+      commit("auth", responseLogin.data.auth);
+      commit("user",responseUser.data.data[0]);
+      router.replace("/home");
+    },
+    logout({commit}) {
+      axios
+      .post("herokuのURL/api/logout", {
+        auth: this.state.auth,
+      })
+      .then((response) => {
+        console.log(response);
+        commit("logout", response.data.auth);
+        router.replace("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    },
   },
-  modules: {
-  }
-})
+});
